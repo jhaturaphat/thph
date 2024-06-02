daa95b1062f4f0d54dd7e7f0180665facf36882b
jhaturaphat <umt.eng52@gmail.com>
2024-06-02 22:06:49 +0700

    FIX

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,  
   // url:'http://110.164.188.212:8080/api'
   //url:'http://110.164.188.213:8080/api'
-  url:"http://backend:8483/api"
+  url:"http://192.168.230.2:8483/api"
 };
