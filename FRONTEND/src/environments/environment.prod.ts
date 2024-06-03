80ff30873f86f98b630bdadd3d38c1f52c3f6a23
jhaturaphat <umt.eng52@gmail.com>
2024-06-03 15:09:18 +0700

    test

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,  
-  // url:'http://110.164.188.212:8080/api'
-  //url:'http://110.164.188.213:8080/api'
-  url:"https://thph.detudomhospital.org/api"
+  url:"thph.detudomhospital.org/api"
+    // url:"https://thph.detudomhospital.org/api"
 };
