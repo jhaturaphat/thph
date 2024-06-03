7169a6fa9e48e3017571b5c1e5eb4e9295356736
jhaturaphat <umt.eng52@gmail.com>
2024-06-03 16:31:25 +0700

    faild

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,  
-  url:"thph.detudomhospital.org/api"
-    // url:"https://thph.detudomhospital.org/api"
+  production: true,    
+    url:"https://thph.detudomhospital.org/api"
 };
